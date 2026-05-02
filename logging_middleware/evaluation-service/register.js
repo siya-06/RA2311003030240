654bd281c7df14f97579a4d0f7169f897f1578bb
siya <siyasri0604@gmail.com>
2026-05-02 11:57:05 +0530

    folder setup

--- logging_middleware/evaluation-service/register.js
+++ logging_middleware/evaluation-service/register.js
@@ -5,46 +5,27 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// app.listen(3000, () => {
-//   console.log('Server running');
-// });
-
-// app.get('/items', (req, res) => {
-//   res.json([]);
-// });
-
-// app.post('/items', (req, res) => {
-//   const data = req.body;
-//   res.status(201).json(data);
-// });
-
-// app.delete('/items/:id', (req, res) => {
-//   const id = req.params.id;
-//   res.json({ message: 'Deleted', id });
-// });
-
-const express = require('express');
-const cors = require('cors');
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
 const PORT = process.env.PORT || 3000;
 
 app.post('/bfhl', (req, res) => {
   const data = req.body.data || [];
 
-  const email = "ur_id";
-  const name = "name";
-  const mobileNo = "urno";
-  const githubusername = "usernamegithub";
-  const rollNo = "urnumber";
+  const email = "ss1132@srmist.edu.in";
+  const name = "Siya Srivastava";
+  const mobileNo = "6389299955";
+  const githubusername = "siya-06";
+  const rollNo = "RA2311003030240";
   const accessCode = "QkbpxH";
+  
 
-  res.status(200).json({ email, name, mobileNo, githubusername, rollNo, accessCode });
+res.status(200).json({ email, name, mobileNo, githubusername, rollNo, accessCode});
 });
 
+app.get('/items', (req, res) => {
+  const clientID = "";
+  const clientSecret = "";
+  res.json([]);
+});
   
 
 
